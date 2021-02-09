e40ed9de24080aa88de885efc478a43d598f60d7
iNKidzy <nadq.nadia@gmail.com>
2021-02-10 00:52:46 +0100

    TimePicker

--- app/src/main/java/com/example/workshop1/MainActivity.kt
+++ app/src/main/java/com/example/workshop1/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.workshop1
 
-import android.content.Intent
-import android.os.Build
+import android.app.TimePickerDialog
 import android.os.Bundle
-import android.support.annotation.RequiresApi
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import android.widget.*
+import java.text.SimpleDateFormat
 import java.util.*
 
 
@@ -17,40 +15,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val linearLayout = findViewById(R.id.linearLayout) as LinearLayout
 
         //Change View
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener {
             val intent = setContentView(R.layout.second)
             startActivity(intent)
-
-
-
-
-         /*  val textView = TextView(this)
-
-           linearLayout.addView(textView)
-
-            val datePicker = DatePicker(this)
-            textView.text = "Year: "+datePicker.year+ " Month: "+ (datePicker.month+1) +" Day: "+ datePicker.dayOfMonth
-
-            val calendar = Calendar.getInstance()
-            calendar.timeInMillis = System.currentTimeMillis()
-
-            datePicker.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH),
-                    DatePicker.OnDateChangedListener { datePicker, year, month, day->
-                        textView.text = "Year: "+ year + " Month: "+ (month+1) + " Day: "+day
-                    })
-            linearLayout.addView(datePicker)
-         */
-
         }
 
     }
 
+    // MainActivity.kt for second.xml
     private fun startActivity(intent: Unit) {
         setContentView(R.layout.second)
+
+
         //DatePicker
         val datePicker = findViewById<DatePicker>(R.id.date_Picker)
         val today = Calendar.getInstance()
@@ -62,10 +41,34 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
 
         }
+
+        val pickTimeBtn = findViewById<Button>(R.id.pickTimeBtn)
+        val timeTV = findViewById<TextView>(R.id.timeTV)
+
+        pickTimeBtn.setOnClickListener{
+            val cal = Calendar.getInstance()
+            val timeSetListener = TimePickerDialog.OnTimeSetListener{timePicker:TimePicker, hour:Int, minute: Int ->
+                cal.set(Calendar.HOUR_OF_DAY, hour)
+                cal.set(Calendar.MINUTE, minute)
+                //time to textview
+                timeTV.text = SimpleDateFormat("HH:mm").format(cal.time)
+            }
+            TimePickerDialog(this, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+
+        }
+
+        //GoBackButton
+        val buttonBack = findViewById<Button>(R.id.buttonBack)
+        buttonBack.setOnClickListener{
+            val intents= setContentView(R.layout.activity_main)
+            backActivity(intents)
+        }
+
     }
 
-    fun selctDate(view: View) {
 
-
+    private fun backActivity(intents: Unit) {
+        setContentView(R.layout.activity_main)
     }
+
 }
