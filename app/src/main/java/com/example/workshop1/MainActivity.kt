80c6217328ff0f25c1eb640c28003485f1a82b88
iNKidzy <nadq.nadia@gmail.com>
2021-02-09 16:37:36 +0100

    design

--- app/src/main/java/com/example/workshop1/MainActivity.kt
+++ app/src/main/java/com/example/workshop1/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.workshop1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
 }
